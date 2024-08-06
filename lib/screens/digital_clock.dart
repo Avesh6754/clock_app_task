b051e872c4a0cce1ecff908cea7d4ee076b43c6a
Avesh6754 <Aweshprajapati150@gmail.com>
2024-08-07 01:52:14 +0530

    07-08-2024

--- lib/screens/digital_clock.dart
+++ lib/screens/digital_clock.dart
@@ -9,7 +9,6 @@ class DigitalClock extends StatefulWidget {
 
   @override
   State<DigitalClock> createState() => _DigitalClockState();
-
 }
 
 class _DigitalClockState extends State<DigitalClock> {
@@ -19,24 +18,26 @@ class _DigitalClockState extends State<DigitalClock> {
     super.initState();
     Timer.periodic(
       Duration(seconds: 1),
-          (timer) {
+      (timer) {
         setState(() {
           future = DateTime.now();
         });
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage('assets/image/vertical-shot-beautiful-starry-sky.jpg'), fit: BoxFit.cover)),
+                image: AssetImage(
+                    '${(name == 'AM') ? background(future) : backgroundPM(future)}'),
+                fit: BoxFit.cover)),
         child: Column(
           children: [
             Spacer(),
@@ -45,22 +46,36 @@ class _DigitalClockState extends State<DigitalClock> {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text(
-                  "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0'):(future.hour).toString().padLeft(2, '0')} : ${future.minute.toString().padLeft(2,'0')} : ${future.second.toString().padLeft(2,'0')}",
+                  "${(future.hour%12==0)?12.toString().padLeft(2,'0'):(future.hour%12).toString().padLeft(2,'0')} : ${future.minute.toString().padLeft(2, '0')}",
                   style: const TextStyle(
-                      fontSize: 60,
+                      fontSize: 70,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
                       fontFamily: 'Digital'),
                 ),
-                SizedBox(width: 10,),
-                Text('${name=(future.hour>12)?'PM':"AM"}',style: TextStyle( fontSize: 30,
+                SizedBox(
+                  width: 10,
+                ),
+                Text('${future.second.toString().padLeft(2, '0')}',style: TextStyle(
+                    fontSize: 30,
                     fontWeight: FontWeight.w600,
                     color: Colors.white,
                     height: -2,
                     fontFamily: 'Digital'),),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  '${name = (future.hour > 12) ? 'PM' : "AM"}',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      height: -2,
+                      fontFamily: 'Digital'),
+                ),
               ],
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -73,13 +88,36 @@ class _DigitalClockState extends State<DigitalClock> {
                         fontFamily: 'Digital'))
               ],
             ),
-Spacer(flex: 4,)
+            Spacer(
+              flex: 4,
+            )
           ],
         ),
       ),
-
     );
   }
 }
 
- var name='0';
+var name = '0';
+
+String background(DateTime future) {
+  final hour = future.hour;
+  if (hour >= 6 && hour < 9) {
+    return 'assets/image/1.gif';
+  } else if (hour >= 9 && hour < 12) {
+    return 'assets/image/2.gif';
+  } else {
+    return 'assets/image/6.gif';
+  }
+}
+
+String backgroundPM(DateTime future) {
+  final hour = future.hour;
+  if (hour >= 7 && hour < 12) {
+    return 'assets/image/5.gif';
+  } else if (hour >= 4 && hour < 7) {
+    return 'assets/image/4.gif';
+  } else {
+    return 'assets/image/3.gif';
+  }
+}
