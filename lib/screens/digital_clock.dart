cda8b9df65da7b0a5445b26d531c294c557d8e2c
Meet Panchal <panchalmeetr97@gmail.com>
2024-08-06 10:37:05 +0530

    06-08-2024

--- lib/screens/digital_clock.dart
+++ lib/screens/digital_clock.dart
@@ -27,69 +27,38 @@ class _DigitalClockState extends State<DigitalClock> {
         width: double.infinity,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage(image[index]), fit: BoxFit.cover)),
-        child: Stack(
-          children:[ Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 25,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0') : (future.hour).toString().padLeft(2, '0')} : ${future.minute} : ${future.second}",
+                image: AssetImage('assets/image/vertical-shot-beautiful-starry-sky.jpg'), fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 25,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0') : (future.hour).toString().padLeft(2, '0')} : ${future.minute} : ${future.second}",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Gupter'),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    '${days[future.weekday - 1]}  ${future.day} ${month[future.month - 1]}',
                     style: TextStyle(
                         fontSize: 40,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
-                        fontFamily: 'Digital'),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                      '${days[future.weekday - 1]}  ${future.day} ${month[future.month - 1]}',
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontFamily: 'Digital'))
-                ],
-              ),
-
-            ],
-          ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (index == 2) {
-                      index = 0;
-                    } else {
-                      index++;
-                    }
-                  });
-                },
-                child: Container(
-                  height: 60,
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: const Text(
-                    'Change Theme',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-              ),
+                        fontFamily: 'Gupter'))
+              ],
             ),
-        ]
+
+          ],
         ),
       ),
 
@@ -120,14 +89,4 @@ List month = [
   'November',
   'December',
 ];
-var index=0;
 
-List image=[
-  'assets/image/bg.jpg',
-
-  'assets/image/bg2.jpg',
-  'assets/image/images.jpg',
-  'assets/image/images (1).jpg',
-  'assets/image/images (2).jpg'
-
-];
