a4ed7b19e083667ce99c7be68ac91a9c21211aa5
Avesh6754 <Aweshprajapati150@gmail.com>
2024-08-06 08:07:55 +0530

    06-08-2024

--- lib/screens/digital_clock.dart
+++ lib/screens/digital_clock.dart
@@ -12,34 +12,122 @@ class DigitalClock extends StatefulWidget {
 class _DigitalClockState extends State<DigitalClock> {
   @override
   Widget build(BuildContext context) {
-    DateTime future= DateTime.now();
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      future=DateTime.now();
-    },);
+    DateTime future = DateTime.now();
+    Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          future = DateTime.now();
+        });
+      },
+    );
     return Scaffold(
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/image/bg.jpg'),
-            fit:
-              BoxFit.cover
-          )
-        ),
-        child: Column(
+            image: DecorationImage(
+                image: AssetImage(image[index]), fit: BoxFit.cover)),
+        child: Stack(
+          children:[ Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 25,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0') : (future.hour).toString().padLeft(2, '0')} : ${future.minute} : ${future.second}",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'Digital'),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      '${days[future.weekday - 1]}  ${future.day} ${month[future.month - 1]}',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Digital'))
+                ],
+              ),
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("${future.hour} : ${future.minute} : ${future.second}",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.white),)
-              ],
-            )
-          ],
+            ],
+          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (index == 2) {
+                      index = 0;
+                    } else {
+                      index++;
+                    }
+                  });
+                },
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(left: 10, right: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Text(
+                    'Change Theme',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+        ]
         ),
       ),
+
     );
   }
 }
+
+List days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+List month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+var index=0;
+
+List image=[
+  'assets/image/bg.jpg',
+
+  'assets/image/bg2.jpg',
+  'assets/image/images.jpg',
+  'assets/image/images (1).jpg',
+  'assets/image/images (2).jpg'
+
+];
