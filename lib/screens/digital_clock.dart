0007333af7c5f651a830bcea76dfabcf50bde047
Avesh6754 <aweshprajapati150@gmail.com>
2024-08-06 13:38:57 +0530

    06-08-2024

--- lib/screens/digital_clock.dart
+++ lib/screens/digital_clock.dart
@@ -2,25 +2,34 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import '../utils/global.dart';
+
 class DigitalClock extends StatefulWidget {
   const DigitalClock({super.key});
 
   @override
   State<DigitalClock> createState() => _DigitalClockState();
+
 }
 
 class _DigitalClockState extends State<DigitalClock> {
   @override
-  Widget build(BuildContext context) {
-    DateTime future = DateTime.now();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
     Timer.periodic(
       Duration(seconds: 1),
-      (timer) {
+          (timer) {
         setState(() {
           future = DateTime.now();
         });
       },
     );
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
     return Scaffold(
       body: Container(
         height: double.infinity,
@@ -29,35 +38,42 @@ class _DigitalClockState extends State<DigitalClock> {
             image: DecorationImage(
                 image: AssetImage('assets/image/vertical-shot-beautiful-starry-sky.jpg'), fit: BoxFit.cover)),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 25,),
+            Spacer(),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text(
-                  "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0') : (future.hour).toString().padLeft(2, '0')} : ${future.minute} : ${future.second}",
-                  style: TextStyle(
-                      fontSize: 40,
+                  "${(future.hour) > 12 ? (future.hour % 12).toString().padLeft(2, '0'):(future.hour).toString().padLeft(2, '0')} : ${future.minute.toString().padLeft(2,'0')} : ${future.second.toString().padLeft(2,'0')}",
+                  style: const TextStyle(
+                      fontSize: 60,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                      fontFamily: 'Gupter'),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    '${days[future.weekday - 1]}  ${future.day} ${month[future.month - 1]}',
-                    style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontFamily: 'Gupter'))
+                      fontFamily: 'Digital'),
+                ),
+                SizedBox(width: 10,),
+                Text('${name=(future.hour>12)?'PM':"AM"}',style: TextStyle( fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    height: -2,
+                    fontFamily: 'Digital'),),
               ],
             ),
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    '${days[future.weekday - 1]} ${future.day} ${month[future.month - 1]}',
+                    style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontFamily: 'Digital'))
+              ],
+            ),
+Spacer(flex: 4,)
           ],
         ),
       ),
@@ -66,27 +82,4 @@ class _DigitalClockState extends State<DigitalClock> {
   }
 }
 
-List days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
-List month = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
+ var name='0';
---
+++ lib/utils/global.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+DateTime future = DateTime.now();
+
+List days = [
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+  'Sun',
+];
+List month = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+List backgrounggift=[
+  'assets/image/1.gif',
+  'assets/image/2.gif',
+  'assets/image/3.gif',
+  'assets/image/4.gif',
+  'assets/image/5.gif',
+  'assets/image/6.gif'
+];
